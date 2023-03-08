3cadfe7a518a0d752f30d49e784170850f68beb9
BrunoCostaPrado <202203973541@alunos.estacio.br>
2023-03-08 04:45:38 -0300

    add api route/ create index files

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,13 @@
 use std::sync::{ Arc, Mutex };
 
-use axum::{ Server, Router, routing::get, extract::State };
+use axum::{ Server, Router, routing::get, extract::State, Json, response::{ IntoResponse, Html } };
 use sysinfo::{ System, SystemExt, CpuExt };
 
 #[tokio::main]
 async fn main() {
     let router = Router::new()
         .route("/", get(root_get))
+        .route("/api/cpus", get(cpus_get))
         .with_state(AppState {
             sys: Arc::new(Mutex::new(System::new())),
         });
@@ -23,19 +24,23 @@ struct AppState {
     sys: Arc<Mutex<System>>,
 }
 
-async fn root_get(State(state): State<AppState>) -> String {
-    use std::fmt::Write;
+#[axum::debug_handler]
+async fn root_get() -> impl IntoResponse {
+    "Hello, World!";
+    Html(include_str!("index.html"))
+}
 
-    let mut s = String::new();
-
+#[axum::debug_handler]
+async fn cpus_get(State(state): State<AppState>) -> impl IntoResponse {
     let mut sys = state.sys.lock().unwrap();
+    //FIXME:Fell bad
     sys.refresh_cpu();
 
-    for (i, cpu) in sys.cpus().iter().enumerate() {
-        let i = i + 1;
+    let v: Vec<_> = sys
+        .cpus()
+        .iter()
+        .map(|cpu| cpu.cpu_usage())
+        .collect();
 
-        let usage = cpu.cpu_usage();
-        writeln!(&mut s, "CPU {i} {usage}%").unwrap();
-    }
-    s
+    Json(v)
 }
